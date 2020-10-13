99a5784b9153e1dd9a5bb7b0d9e5e5883a9555af
Haben <nik.bersenev0@gmail.com>
2020-10-13 20:44:00 +0400

    _

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,13 +1,39 @@
 module.exports.homelist = function (req, res, next) {
-    res.render('locations-list', { title: 'Home' });
+    res.render('locations-list', {
+        title: 'Loc8r - find a place to work with wifi',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        locations:[
+            {name:'Starcups',
+            address:'125 High Street, Reading, RG6 1PS',
+            rating:4,
+            facilities:['Hot drinks','Food','Premium wifi'],
+            distance:'100m'},
+            {name:'Cafe Hero',
+            address:'125 High Street, Reading, RG6 1PS',
+            rating:2,
+            facilities:['Hot drinks','Food','Premium wifi'],
+            distance:'200m'},
+            {name:'Burger Queen',
+            address:'125 High Street, Reading, RG6 1PS',
+            rating:3,
+            facilities:['Food','Premium wifi'],
+            distance:'250m'},
+        ]
+    });
+
 }
 
 module.exports.locationInfo = function (req, res, next) {
-    res.render('location-info', { title: 'Location info' });
+    res.render('location-info', {
+        title: 'Location info'
+    });
 }
 
 module.exports.addReview = function (req, res, next) {
-    res.render('location-review-form', { title: 'Add review' });
+    res.render('location-review-form', {
+        title: 'Add review'
+    });
 }
-
- 
